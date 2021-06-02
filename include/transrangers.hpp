fed283aa4e460c0b352fe9e6decc4109fad106aa
joaquintides <joaquin@tid.es>
2021-06-02 16:00:39 +0200

    eliminated first step in unique

--- include/transrangers.hpp
+++ include/transrangers.hpp
@@ -157,6 +157,7 @@ auto unique(Ranger rgr)
   using cursor=typename Ranger::cursor;
     
   return ranger<cursor>([=,start=true,p=cursor{}](auto dst)mutable{
+#if 0
     if(start){
       start=false;
       bool cont=false;
@@ -168,10 +169,19 @@ auto unique(Ranger rgr)
       if(!cont)return false;
     }
     return rgr([&](auto q){
-      auto pp=p;
+      auto prev_p=p;
       p=q;
-      return *pp==*q?true:dst(q);
+      return *prev_p==*q?true:dst(q);
     });
+#else
+    return rgr([&](auto q){
+      auto prev_p=p;
+      p=q;
+      auto prev_start=start;
+      start=false;
+      return !prev_start&&*prev_p==*q?true:dst(q);
+    });
+#endif
   });
 }
 
