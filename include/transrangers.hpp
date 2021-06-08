23f1556778808b29c085350b7e6e645e5b88d56b
joaquintides <joaquin@tid.es>
2021-06-08 21:25:26 +0200

    reverted (unnecessary)

--- include/transrangers.hpp
+++ include/transrangers.hpp
@@ -45,7 +45,6 @@ auto all(Range&& rng)
   return ranger<cursor>([first=begin(rng),last=end(rng)](auto dst)mutable{
     auto it=first;
     while(it!=last)if(!dst(it++)){first=it;return false;}
-    first=it;
     return true;
   });
 }
