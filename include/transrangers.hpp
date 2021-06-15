ddc5e1a177c34f9eb6a6a8d36dfac48718f07db9
joaquintides <joaquin@tid.es>
2021-06-15 11:41:54 +0200

    refactored ranger_join

--- include/transrangers.hpp
+++ include/transrangers.hpp
@@ -194,11 +194,12 @@ auto unique(Ranger rgr)
   });
 }
 
-template<typename Ranger>
-auto join(Ranger rgr)
+template<typename Ranger,typename Adaptor>
+auto join(Ranger rgr,Adaptor adaptor)
 {
   using cursor=typename Ranger::cursor;
-  using subranger=std::remove_cvref_t<decltype(*std::declval<cursor>())>; 
+  using subranger=std::remove_cvref_t<
+    decltype(adaptor(*std::declval<const cursor&>()))>; 
   using subranger_cursor=typename subranger::cursor;
     
   return ranger<subranger_cursor>(
@@ -208,7 +209,7 @@ auto join(Ranger rgr)
       if(!(*osrgr)(dst))return false;
     }
     return rgr([&](const auto& p) TRANSRANGERS_HOT {
-      auto srgr=*p;
+      auto srgr=adaptor(*p);
       if(!srgr(dst)){
         osrgr.emplace(std::move(srgr));
         return false;
@@ -219,27 +220,23 @@ auto join(Ranger rgr)
 }
 
 template<typename Ranger>
+auto join(Ranger rgr)
+{
+  auto identity_adaptor=[](auto&& rgr){
+    return std::forward<decltype(rgr)>(rgr);
+  };
+  
+  return join(std::move(rgr),identity_adaptor);
+}
+
+template<typename Ranger>
 auto ranger_join(Ranger rgr)
 {
-  using cursor=typename Ranger::cursor;
-  using subranger=decltype(all(*std::declval<cursor>())); 
-  using subranger_cursor=typename subranger::cursor;
-    
-  return ranger<subranger_cursor>(
-    [=,osrgr=std::optional<subranger>{}]
-    (auto dst) TRANSRANGERS_HOT_MUTABLE {
-    if(osrgr){
-      if(!(*osrgr)(dst))return false;
-    }
-    return rgr([&](const auto& p) TRANSRANGERS_HOT {
-      auto srgr=all(*p);
-      if(!srgr(dst)){
-        osrgr.emplace(std::move(srgr));
-        return false;
-      }
-      else return true;
-    });
-  });
+  auto all_adaptor=[](auto&& srng){
+    return all(std::forward<decltype(srng)>(srng));
+  };
+
+  return join(std::move(rgr),all_adaptor);
 }
 
 template<typename... Rangers>
